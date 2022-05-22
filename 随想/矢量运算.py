aa38da2b8310700907ae790f152a60fe1194ef27
WangHao <1046911101@qq.com>
2022-05-22 23:34:25 +0800

    fix requirements.txt

--- 随想/矢量运算.py
+++ 随想/矢量运算.py
@@ -1,8 +1,8 @@
 import time
 import numpy as np
+import cython
 import torch
 import tensorflow as tf
-from numba import jit, njit
 
 
 def execute_time(func):
@@ -29,6 +29,24 @@ def python_dot(a, b):
             c[i][j] = s
     return c
 
+@execute_time
+@cython.cfunc
+def cython_dot(a, b):
+    if len(a) != len(b[0]):
+        raise ValueError('shape not matched')
+    n: cython.int = len(a)
+    p: cython.int = len(a[0])
+    m: cython.int = len(b[0])
+    c = [[0 for _ in range(n)] for _ in range(m)]
+    i: cython.int = 0
+    j: cython.int = 0
+    for i in range(m):
+        for j in range(n):
+            s = 0
+            for k in range(p):
+                s += a[i][k] * b[k][j]
+            c[i][j] = s
+    return c
 
 @execute_time
 def numpy_dot(a, b):
@@ -54,35 +72,43 @@ def tensorflow_dot(a, b):
     return c
 
 
-@execute_time
-@jit(nopython=True)
-def numba_dot_1(a, b):
-    if a.shape[1] != b.shape[0]:
-        raise ValueError('shape not matched')
-    n, p, m = a.shape[0], a.shape[1], b.shape[1]
-    c = np.zeros((n, m), dtype=np.float32)
-    for i in range(n):
-        for j in range(m):
-            s = 0
-            for k in range(p):
-                s += a[i, k] * b[k, j]
-            c[i, j] = s
-    return c
+# from numba import jit, njit
+# @execute_time
+# @jit(nopython=True)
+# def numba_dot_1(a, b):
+#     if a.shape[1] != b.shape[0]:
+#         raise ValueError('shape not matched')
+#     n, p, m = a.shape[0], a.shape[1], b.shape[1]
+#     c = np.zeros((n, m), dtype=np.float32)
+#     for i in range(n):
+#         for j in range(m):
+#             s = 0
+#             for k in range(p):
+#                 s += a[i, k] * b[k, j]
+#             c[i, j] = s
+#     return c
 
+# @execute_time
+# @njit
+# def numba_dot_2(a, b):
+#     if a.shape[1] != b.shape[0]:
+#         raise ValueError('shape not matched')
+#     c = np.dot(a, b)
+#     return c
 
-@execute_time
-@njit
-def numba_dot_2(a, b):
-    if a.shape[1] != b.shape[0]:
-        raise ValueError('shape not matched')
-    c = np.dot(a, b)
-    return c
+# a = np.ones((1000, 500))
+# b = np.ones((500, 1000))
+# numba_dot_1(a, b)
+# numba_dot_1(a, b)
+# numba_dot_2(a, b)
+# numba_dot_2(a, b)
 
 
 if __name__ == "__main__":
     a = [[0 for i in range(1000)] for j in range(500)]
     b = [[0 for i in range(500)] for j in range(1000)]
-    python_dot(a, b)
+    # python_dot(a, b)
+    # cython_dot(a, b)
 
     a = np.random.rand(1000, 500)
     b = np.random.rand(500, 1000)
@@ -95,11 +121,3 @@ if __name__ == "__main__":
     a = tf.random.normal((1000, 500))
     b = tf.random.normal((500, 1000))
     tensorflow_dot(a, b)
-
-    a = np.ones((1000, 500))
-    b = np.ones((500, 1000))
-    numba_dot_1(a, b)
-    numba_dot_1(a, b)
-    numba_dot_2(a, b)
-    numba_dot_2(a, b)
-

b7e77e337eeb73ad3a16310105f5d78f82600d9a
WangHao <1046911101@qq.com>
2022-09-06 08:53:09 +0800

    fixed floder

--- 练习/上理/29.py
+++ 练习/上理/29.py
@@ -1,3 +1,4 @@
+import time
 import torch.nn as nn
 
 class DoubleConv(nn.Module):
@@ -17,18 +18,32 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         x = self.double_conv(x)
         return x
-    
- 
-class MyConv(nn.Module):   
+
+
+class MyConv(nn.Module):
     def __init__(self):
         super().__init__()
         """下面参数是假设"""
         conv1 = DoubleConv(128, 64, 128)
         conv2 = DoubleConv(128, 64, 128)
         conv3 = DoubleConv(128, 64, 128)
-     
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        return x                 
+        return x
+
+
+import numpy as np
+import matplotlib.pyplot as plot
+fig = plot.figure(1, figsize=(2, 2), dpi=300)
+image = np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]])
+plot.imshow(image, cmap='gray')
+plot.margins(0., 0.)  # 让轴中的内容紧贴轴
+plot.axis('off')
+plot.subplots_adjust(0., 0., 1., 1., 0., 0.)  # 让全部轴所处的区域与图完全重合
+fig.savefig('image.tiff', bbox_inches='tight', pad_inches=0., transparent=True)
+plot.show()
+time.sleep(5)
+plot.close(fig=fig)

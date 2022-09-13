c5c6533f79fde8ec8bd52b9963ac824d287a23b6
WangHao <1046911101@qq.com>
2022-09-14 01:48:38 +0800

    daily maintenance

---
+++ 算法/编程笔试/中兴秋招0913/sunject02.py
@@ -0,0 +1,15 @@
+import sys
+
+
+class Solution:
+
+    def hashmap_set(self, strings_1, strings_2):
+        strings_1 = set(list(strings_1))
+        strings_2 = set(list(strings_2))
+
+        return (strings_1 ^ strings_2).pop()
+
+
+get_strings = list(map(str, sys.stdin.readline().strip().split(' ')))
+result = Solution().hashmap_set(get_strings[0], get_strings[1])
+sys.stdout.write(f"{result}\n")

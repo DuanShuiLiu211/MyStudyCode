bc6860af55ed15cf58f6999556079bc9ad797956
WangHao <1046911101@qq.com>
2022-09-14 21:56:00 +0800

    fixed details

---
+++ 算法/编程笔试/小米秋招0914.py
@@ -0,0 +1,30 @@
+import sys
+import collections
+
+
+class Solution:
+    def min_operations(self, nums, x):
+        sum_nums = sum(nums)
+        if sum_nums == x:
+            return len(nums)
+        last = sum_nums - x
+        pre = [0]
+        for i in range(len(nums)):
+            pre.append(pre[-1] + nums[i])
+        res = float("-inf")
+        dic = collections.defaultdict(int)
+        for index, value in enumerate(pre):
+            if value - last in dic:
+                res = max(res, index - dic[value - last])
+            if value not in dic:
+                dic[value] = index
+        return -1 if res == float("-inf") else len(nums) - res
+
+get_strings = []
+get_strings.append(list(map(int, sys.stdin.readline().replace("[", "").replace("]", "").split(","))))
+get_strings.append(int(sys.stdin.readline().strip()))
+get_strings.append(int(sys.stdin.readline().strip()))
+
+
+result = Solution().min_operations(get_strings[0], get_strings[-1])
+print(result)
---
+++ 随想/小经验/PyTorchVein/utils/FullFeedNet.py
@@ -0,0 +1,28 @@
+from torch import Tensor
+from torch import nn
+from einops import rearrange
+
+
+class FFN(nn.Module):
+    def __init__(self,
+                 data_size: int = 240,
+                 layer_size: int = 3
+                 ) -> None:
+        super().__init__()
+        self.data_size = data_size
+        self.channels = data_size**2
+        ffn_layers = []
+        for _ in range(layer_size):
+            ffn_layers.append(nn.Linear(self.channels, self.channels))
+        self.ffn_layers = nn.Sequential(*ffn_layers)
+        self.to_out = nn.Sigmoid()
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        assert len(inputs.shape) == 4
+        b, c, h, w = inputs.shape
+        inputs = rearrange(inputs, 'b c h w -> b c (h w)', b=b, c=c, h=h, w=w)
+        outputs = self.ffn_layers(inputs)
+        outputs = self.to_out(outputs)
+        outputs = rearrange(outputs, 'b c hw -> b c h w', b=b, c=c, h=h, w=w)
+
+        return outputs

4079ef05679a75b643259b1ca73942d7edd08304
WangHao <1046911101@qq.com>
2022-09-15 13:14:56 +0800

    fixed details

--- 随想/小经验/JaxVein/Jax0.x.py
+++ 随想/小经验/JaxVein/Jax0.x.py
@@ -20,7 +20,10 @@ class ResNetBlock(nn.Module):
     strides: Tuple[int, int] = (1, 1)
 
     @nn.compact
-    def __call__(self, x, ):
+    def __call__(
+        self,
+        x,
+    ):
         residual = x
         y = self.conv(self.filters, (3, 3), self.strides)(x)
         y = self.norm()(y)
@@ -30,7 +33,8 @@ class ResNetBlock(nn.Module):
 
         if residual.shape != y.shape:
             residual = self.conv(self.filters, (1, 1),
-                                 self.strides, name='conv_proj')(residual)
+                                 self.strides,
+                                 name='conv_proj')(residual)
             residual = self.norm(name='norm_proj')(residual)
 
         return self.act(residual + y)
@@ -59,7 +63,8 @@ class BottleneckResNetBlock(nn.Module):
 
         if residual.shape != y.shape:
             residual = self.conv(self.filters * 4, (1, 1),
-                                 self.strides, name='conv_proj')(residual)
+                                 self.strides,
+                                 name='conv_proj')(residual)
             residual = self.norm(name='norm_proj')(residual)
 
         return self.act(residual + y)
@@ -94,7 +99,7 @@ class ResNet(nn.Module):
         for i, block_size in enumerate(self.stage_sizes):
             for j in range(block_size):
                 strides = (2, 2) if i > 0 and j == 0 else (1, 1)
-                x = self.block_cls(self.num_filters * 2 ** i,
+                x = self.block_cls(self.num_filters * 2**i,
                                    strides=strides,
                                    conv=conv,
                                    norm=norm,
@@ -105,8 +110,10 @@ class ResNet(nn.Module):
         return x
 
 
-ResNet18_c10 = partial(ResNet, stage_sizes=[2, 2, 2, 2],
-                       block_cls=ResNetBlock, num_classes=10)
+ResNet18_c10 = partial(ResNet,
+                       stage_sizes=[2, 2, 2, 2],
+                       block_cls=ResNetBlock,
+                       num_classes=10)
 
 
 # 定义数据流
@@ -114,30 +121,31 @@ def data_flow(*, dataset, batch_size=1, prng=None):
     total_data = len(dataset)
     if prng is not None:
         index_order = np.array(range(total_data))
-        index_shuffle = jax.random.permutation(prng, index_order, independent=True)
+        index_shuffle = jax.random.permutation(prng,
+                                               index_order,
+                                               independent=True)
     else:
         index_order = np.array(range(total_data))
         index_shuffle = index_order
 
     total_batch = total_data // batch_size
     for idx in range(total_batch):
-        batch_index = index_shuffle[idx * batch_size: (idx + 1) * batch_size]
+        batch_index = index_shuffle[idx * batch_size:(idx + 1) * batch_size]
         mini_batch = [dataset[k] for k in batch_index]
-        images = np.expand_dims(np.stack([x['image'] for x in mini_batch]), -1).astype('float') / 255
+        images = np.expand_dims(np.stack([x['image'] for x in mini_batch]),
+                                -1).astype('float') / 255
         labels = np.stack([x['label'] for x in mini_batch])
-        yield {
-            'image': images,
-            'label': labels
-        }
+        yield {'image': images, 'label': labels}
 
 
-dataset_mnist = np.load("dataset/mnist.npy", allow_pickle=True).item()
+dataset_mnist = np.load("datasets/mnist.npy", allow_pickle=True).item()
 
 
 # 定义损失函数
 def cross_entropy_loss(*, logits, labels):
     labels_onehot = jax.nn.one_hot(labels, num_classes=10)
-    return optax.softmax_cross_entropy(logits=logits, labels=labels_onehot).mean()
+    return optax.softmax_cross_entropy(logits=logits,
+                                       labels=labels_onehot).mean()
 
 
 # 定义评估指标
@@ -156,7 +164,9 @@ def create_train_state(*, prng, learning_rate, momentum):
     net = ResNet18_c10()
     params = net.init(prng, jnp.ones([1, 28, 28, 1]))['params']
     tx = optax.sgd(learning_rate, momentum)
-    return train_state.TrainState.create(apply_fn=net.apply, params=params, tx=tx)
+    return train_state.TrainState.create(apply_fn=net.apply,
+                                         params=params,
+                                         tx=tx)
 
 
 # 定义训练方法
@@ -167,8 +177,11 @@ def train_step(state, batch_data):
     state: 不仅包含参数信息还包含优化器的信息等
     batch_data: 批数据 (N, H, W, C)
     """
+
     def loss_fn(params):
-        logits, _ = ResNet18_c10().apply({'params': params}, batch_data['image'], mutable=['batch_stats'])
+        logits, _ = ResNet18_c10().apply({'params': params},
+                                         batch_data['image'],
+                                         mutable=['batch_stats'])
         loss = cross_entropy_loss(logits=logits, labels=batch_data['label'])
         return loss, logits
 
@@ -185,15 +198,22 @@ def train_model(state, epoch, batch_size, prng):
     train_dataset = dataset_mnist['train']
     total_batch = len(train_dataset) // batch_size
 
-    with tqdm(data_flow(dataset=train_dataset, batch_size=batch_size, prng=prng), total=total_batch) as run_bar_set:
+    with tqdm(data_flow(dataset=train_dataset,
+                        batch_size=batch_size,
+                        prng=prng),
+              total=total_batch) as run_bar_set:
         for batch in run_bar_set:
             state, metrics = train_step(state, batch)
             batch_metrics.append(metrics)
             batch_metrics_jnp = jax.device_get(batch_metrics)
-            epoch_metrics = {k: np.mean([metrics[k] for metrics in batch_metrics_jnp]) for k in metrics.keys()}
-            run_bar_set.set_description(f"train epoch: {epoch+1}, "
-                                        f"loss: {epoch_metrics['loss']:.4f}, "
-                                        f"accuracy: {(epoch_metrics['accuracy'] * 100):.2f}")
+            epoch_metrics = {
+                k: np.mean([metrics[k] for metrics in batch_metrics_jnp])
+                for k in metrics.keys()
+            }
+            run_bar_set.set_description(
+                f"train epoch: {epoch+1}, "
+                f"loss: {epoch_metrics['loss']:.4f}, "
+                f"accuracy: {(epoch_metrics['accuracy'] * 100):.2f}")
 
     return state
 
@@ -206,7 +226,9 @@ def test_step(params, batch_data):
     params: 经过训练的参数
     batch_data: 批数据 (N, H, W, C)
     """
-    logits, _ = ResNet18_c10().apply({'params': params}, batch_data['image'], mutable=['batch_stats'])
+    logits, _ = ResNet18_c10().apply({'params': params},
+                                     batch_data['image'],
+                                     mutable=['batch_stats'])
     return compute_metrics(logits=logits, labels=batch_data['label'])
 
 
@@ -216,15 +238,20 @@ def test_model(params, epoch, batch_size):
     test_dataset = dataset_mnist['test']
     total_batch = len(test_dataset) // batch_size
 
-    with tqdm(data_flow(dataset=test_dataset, batch_size=batch_size), total=total_batch) as run_bar_set:
+    with tqdm(data_flow(dataset=test_dataset, batch_size=batch_size),
+              total=total_batch) as run_bar_set:
         for batch in run_bar_set:
             metrics = test_step(params, batch)
             batch_metrics.append(metrics)
             batch_metrics_jnp = jax.device_get(batch_metrics)
-            epoch_metrics = {k: np.mean([metrics[k] for metrics in batch_metrics_jnp]) for k in metrics.keys()}
-            run_bar_set.set_description(f"train epoch: {epoch+1}, "
-                                        f"loss: {epoch_metrics['loss']:.4f}, "
-                                        f"accuracy: {(epoch_metrics['accuracy'] * 100):.2f}")
+            epoch_metrics = {
+                k: np.mean([metrics[k] for metrics in batch_metrics_jnp])
+                for k in metrics.keys()
+            }
+            run_bar_set.set_description(
+                f"train epoch: {epoch+1}, "
+                f"loss: {epoch_metrics['loss']:.4f}, "
+                f"accuracy: {(epoch_metrics['accuracy'] * 100):.2f}")
 
     return epoch_metrics
 
@@ -232,21 +259,27 @@ def test_model(params, epoch, batch_size):
 # 进行训练与测试
 seed = 51
 prng = jax.random.PRNGKey(seed)  # 通过种子获取随机数生成器密钥
-prng, init_prng = jax.random.split(prng, 2)  # 拆分原随机数生成器密钥得到2个新的密钥，使用相同密钥随机函数将输出相同结果，用其实现可复现的权重初始化
+prng, init_prng = jax.random.split(
+    prng, 2)  # 拆分原随机数生成器密钥得到2个新的密钥，使用相同密钥随机函数将输出相同结果，用其实现可复现的权重初始化
 num_epochs = 10
 batch_size = 32
 learning_rate = 0.1
 momentum = 0.9
-state = create_train_state(prng=init_prng, learning_rate=learning_rate, momentum=momentum)
+state = create_train_state(prng=init_prng,
+                           learning_rate=learning_rate,
+                           momentum=momentum)
 
 for epoch in range(num_epochs):
     # 定义用于打乱数据顺序的伪随机数生成器
     prng, data_prng = jax.random.split(prng)
-    for train_batch_data in data_flow(dataset=dataset_mnist['train'], batch_size=batch_size, prng=data_prng):
+    for train_batch_data in data_flow(dataset=dataset_mnist['train'],
+                                      batch_size=batch_size,
+                                      prng=data_prng):
         print(train_batch_data['image'].shape, train_batch_data['image'].dtype)
         print(train_batch_data['label'].shape, train_batch_data['label'].dtype)
         break
-    for test_batch_data in data_flow(dataset=dataset_mnist['test'], batch_size=batch_size):
+    for test_batch_data in data_flow(dataset=dataset_mnist['test'],
+                                     batch_size=batch_size):
         print(test_batch_data['image'].shape, test_batch_data['image'].dtype)
         print(test_batch_data['label'].shape, test_batch_data['label'].dtype)
         break
--- 随想/小经验/PyTorchVein/PyTorch1.x.py
+++ 随想/小经验/PyTorchVein/PyTorch1.x.py
@@ -5,18 +5,16 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-
-
 """定义数据"""
 training_data = datasets.FashionMNIST(
-    root="./dataset",
+    root="./datasets",
     train=True,
     download=True,
     transform=ToTensor(),
 )
 
 test_data = datasets.FashionMNIST(
-    root="./dataset",
+    root="./datasets",
     train=False,
     download=True,
     transform=ToTensor(),
@@ -31,8 +29,6 @@ for X, y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
-
-
 """定义模型"""
 if platform.system() == 'Darwin':
     device = "mps" if torch.backends.mps.is_available() else "cpu"
@@ -42,13 +38,14 @@ print(f"Using {device} device")
 
 
 class NeuralNetwork(nn.Module):
+
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.conv = torch.nn.Conv2d(1, 32, (3, 3), padding=1)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(32*28*28, 128),
+            nn.Linear(32 * 28 * 28, 128),
             nn.ReLU(),
             nn.Linear(128, 10),
         )
@@ -62,8 +59,6 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device)
 print(model)
-
-
 """优化模型"""
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
@@ -100,7 +95,9 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
 
 
 if __name__ == "__main__":
@@ -118,7 +115,6 @@ if __name__ == "__main__":
             torch.save(model.state_dict(), "model.pth")
             print("Saved PyTorch Model State to model.pth")
     print("Done!")
-
     """验证模型"""
     verify_model = False
     if verify_model:
@@ -126,16 +122,16 @@ if __name__ == "__main__":
         model.load_state_dict(torch.load("model.pth"))
 
         classes = [
-        "T-shirt/top",
-        "Trouser",
-        "Pullover",
-        "Dress",
-        "Coat",
-        "Sandal",
-        "Shirt",
-        "Sneaker",
-        "Bag",
-        "Ankle boot",
+            "T-shirt/top",
+            "Trouser",
+            "Pullover",
+            "Dress",
+            "Coat",
+            "Sandal",
+            "Shirt",
+            "Sneaker",
+            "Bag",
+            "Ankle boot",
         ]
 
         model.eval()

9c1ccca1dcd7cfa0e3c05207edfafd7e9c76e84f
WangHao <1046911101@qq.com>
2022-09-19 18:41:52 +0800

    fixed details

--- 随想/小技巧/屏幕截图.py
+++ 随想/小技巧/屏幕截图.py
@@ -1,11 +1,24 @@
 from PIL import ImageGrab
 import platform
+import time
+
+
+def z(f):
+    def y(*x):
+        start_time = time.time()
+        f(*x)  # 被包装的对象
+        stop_time = time.time()
+        delta_time = stop_time - start_time
+        return print('{:.9f}'.format(delta_time))
+
+    return y
 
 
 if platform.system() == "Windows":
 
     import win32gui, win32ui, win32con, win32api
 
+    @z
     def window_capture(filename, r, c, w, h):
         # 窗口的编号，0号表示当前活跃窗口
         hwnd = 0
@@ -26,14 +39,16 @@ if platform.system() == "Windows":
         saveDC.SelectObject(saveBitMap)
         # 截取从左上角（0，0）长宽为（w，h）的图片
         saveDC.BitBlt((0, 0), (w, h), mfcDC, (r, c), win32con.SRCCOPY)
-        saveBitMap.SaveBitmapFile(saveDC, filename)
+        # saveBitMap.SaveBitmapFile(saveDC, filename)
 
     window_capture("./2.png", 0, 0, 1980, 1024)
 
 else:
+    @z
     def python_capture(filename, *place):
         screen = ImageGrab.grab()
         print("显示分辨率:{}".format(screen.size))
         img = ImageGrab.grab(bbox=place)
         img.save(filename)
+
     python_capture("./2.png", 0, 0, 1980, 1024)

cf35fb443821328cf6138057abed9c0e3c1e8c97
WangHao <1046911101@qq.com>
2022-09-20 17:15:58 +0800

    fixed

--- 练习/微创/demo.py
+++ 练习/微创/demo.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import os
 
 
 if __name__ == "__main__":
-    file_dir_1 = r'/Users/WangHao/Desktop/TODO/Data/data_divide.xlsx'
-    pd_sheet_1 = pd.read_excel(file_dir_1)
-    pd_data_1 = pd_sheet_1.values[:, 1]
-    list_data_1 = sorted(list(pd_data_1))
+    file_dir_1 = r'/Volumes/昊大侠/工作/实习相关/微创卜算子医疗科技有限公司/陈嘉懿组/数据/短轴动态狭窄率/result/狭窄率图表_0916/results/rvm'
+    pd_sheet_1 = os.listdir(file_dir_1)
+    list_data_1 = sorted(pd_sheet_1)
     set_data_1 = set(list_data_1)
 
-    file_dir_2 = r'/Volumes/昊大侠/工作/实习相关/微创卜算子医疗科技有限公司/陈嘉懿组/数据/短轴动态狭窄率/result/狭窄率图表_0916/rvm/data_divide.xlsx'
-    pd_sheet_2 = pd.read_excel(file_dir_2)
-    pd_data_2 = pd_sheet_2.values[:, 1]
-    list_data_2 = sorted(list(pd_data_2))
+    file_dir_2 = r'/Volumes/昊大侠/工作/实习相关/微创卜算子医疗科技有限公司/陈嘉懿组/数据/短轴动态狭窄率/result/狭窄率图表_0916/results/transbts'
+    pd_sheet_2 = os.listdir(file_dir_2)
+    # pd_sheet_2.remove('.DS_Store')
+    # pd_sheet_2.remove('._.DS_Store')
+    list_data_2 = sorted(pd_sheet_2)
     set_data_2 = set(list_data_2)
 
     print(f"diffrent data {set_data_1 ^ set_data_2}")

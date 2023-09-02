92481f8e1135b36075d0fbf6e9030aa5c28e785a
WangHao <1046911101@qq.com>
2023-09-02 15:33:55 +0800

    [Normal]: over coroutine

--- 小练习/图丽/5.py
+++ 小练习/图丽/5.py
@@ -1,19 +1,25 @@
 import asyncio
-import time
 
+async def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+        await asyncio.sleep(0.1) 
 
-async def say_after(delay, what):
-    await asyncio.sleep(delay)
-    print(what)
-
+async def print_dots():
+    for _ in range(20):  # 打印20个点
+        print(".", end="", flush=True)
+        await asyncio.sleep(0.05)
 
 async def main():
-    print(f"started at {time.strftime('%X')}")
+    task1 = asyncio.create_task(print_dots())
+    
+    # 直接在这里迭代fib函数
+    async for value in fib(10):
+        print(value)
+    
+    # 等待print_dots完成
+    await task1
 
-    await say_after(1, 'hello')
-    await say_after(2, 'world')
-
-    print(f"finished at {time.strftime('%X')}")
-
-
-asyncio.run(main())  # 花费时间为3秒
+asyncio.run(main())

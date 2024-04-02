ee6341551d6f27788bcbf58ce5eca31718ec1a3f
WangHao <1046911101@qq.com>
2024-04-02 09:30:00 +0800

    [Normal]: 20240402

---
+++ 小随想/技巧/装饰器集市.py
@@ -0,0 +1,26 @@
+import functools
+import time
+
+
+# 一个使用条件生存装饰器的例子
+def take_time(threshold):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            func(*args, **kwargs)
+            end_time = time.time()
+            if end_time - start_time > threshold:
+                print(f"{func.__name__}函数运行时间超过了{threshold}秒")
+
+        return wrapper
+
+    return decorator
+
+
+@take_time(1)
+def sleep(secs):
+    time.sleep(secs)
+
+
+sleep(2)

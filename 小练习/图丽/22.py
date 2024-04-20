8f754c641a4719068043d2bbd1412c0937f9d8b2
WangHao <1046911101@qq.com>
2024-04-20 10:12:27 +0800

    [Normal]: 20240420

--- 小练习/图丽/22.py
+++ 小练习/图丽/22.py
@@ -1,31 +1,29 @@
 import multiprocessing
 import signal
 
+event = multiprocessing.Event()
 
-def child_process(flag):
-    # 注册信号处理函数
-    def signal_handler(signal_number, code_frame):
-        # 在这里定义当信号被触发时要执行的操作
-        print("Signal handler called with signal", signal_number)
-        print("Code frame:", code_frame)
-        flag.value = False
+def signal_handler(signal_number, code_frame):
+    print("Signal handler called with signal", signal_number)
+    print("Code frame:", code_frame)
+    event.set()
 
-    signal.signal(signal.SIGINT, signal_handler)  # 注册 Ctrl+C 信号处理程序
 
-    # 无限循环来保持程序运行，直到 flag 的值为 False
+def child_process(event):
     print("Press Ctrl+C to trigger the signal handler...")
-    while flag.value:
+    while not event.is_set():
         print("Waiting for signal...")
+        event.wait(timeout=1)
 
 
 def main():
-    # 创建共享标志变量
-    flag = multiprocessing.Value("b", True)
+    # 设置信号处理程序
+    signal.signal(signal.SIGINT, signal_handler)
 
     # 创建多个子进程
     processes = []
     for _ in range(5):
-        process = multiprocessing.Process(target=child_process, args=(flag,))
+        process = multiprocessing.Process(target=child_process, args=(event,))
         processes.append(process)
 
     # 启动子进程

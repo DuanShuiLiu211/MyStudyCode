7350bdeaf092352c3649f72950c748e374320cce
WangHao <1046911101@qq.com>
2024-04-20 11:36:54 +0800

    [Normal]: 20240420

--- 小练习/图丽/rtsp.py
+++ 小练习/图丽/rtsp.py
@@ -16,12 +16,12 @@ def get_args():
     parser.add_argument(
         "--input_file",
         default="/Users/WangHao/Sites/工作/图丽信息/程序代码/东明停车/出入口数据/出口/出口20240123_105118_14.mkv",
-        help="程序的主要配置文件",
+        help="输入文件",
     )
     parser.add_argument(
         "--output_url",
         default="rtsp://localhost:8554/mystream",
-        help="程序的主要配置文件",
+        help="输出链接",
     )
 
     return parser.parse_args()

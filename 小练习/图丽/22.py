75ee7b6a3dcdefd003bab3dffe3a9073ca7d74aa
WangHao <1046911101@qq.com>
2024-04-20 11:26:12 +0800

    [Normal]: 20240420

--- 小练习/图丽/22.py
+++ 小练习/图丽/22.py
@@ -1,41 +1,46 @@
 import multiprocessing
+import os
 import signal
+import time
 
-event = multiprocessing.Event()
 
-def signal_handler(signal_number, code_frame):
-    print("Signal handler called with signal", signal_number)
-    print("Code frame:", code_frame)
-    event.set()
+def signal_handler(event):
+    def handler(signal_number, code_frame):
+        print("进程号", os.getpid(), "事件号", id(event), "事件状态", event.is_set())
+        print("信号类型", signal_number)
+        print("代码帧", code_frame)
+        event.set()
+        print("进程号", os.getpid(), "事件号", id(event), "事件状态", event.is_set())
+
+    return handler
 
 
 def child_process(event):
-    signal.signal(signal.SIGINT, signal_handler)
-    print("Press Ctrl+C to trigger the signal handler...")
+    signal.signal(signal.SIGINT, signal_handler(event))
+    signal.signal(signal.SIGTERM, signal_handler(event))
     while not event.is_set():
-        print("Waiting for signal...")
-        event.wait(timeout=1)
+        print("子进程号", os.getpid(), "事件号", id(event), "事件状态", event.is_set())
+        time.sleep(1)
 
 
 def main():
-    # 设置信号处理程序
-    signal.signal(signal.SIGINT, signal_handler)
-
-    # 创建多个子进程
+    event = multiprocessing.Event()
+    signal.signal(signal.SIGINT, signal_handler(event))
+    signal.signal(signal.SIGTERM, signal_handler(event))
+    print("主进程号", os.getpid(), "事件号", id(event))
     processes = []
     for _ in range(5):
         process = multiprocessing.Process(target=child_process, args=(event,))
         processes.append(process)
 
-    # 启动子进程
     for process in processes:
         process.start()
 
-    # 等待所有子进程结束
     for process in processes:
         process.join()
 
 
 if __name__ == "__main__":
+    print("程序开始")
     main()
-    print("Exiting…")
+    print("程序结束")
